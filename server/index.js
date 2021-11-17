c3848167c91f07c6b677daf31debe03243a9d2ca
JoshDaos <joshuadaos1998@gmail.com>
2021-11-16 18:27:43 -0800

    express setup & connecting to mongodb

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://user123:test123@mern-tut.lx6oz.mongodb.net/merntutorial?retryWrites=true&w=majority');
+
+app.listen(7001, () => {
+    console.log('Server is running..')
+});
