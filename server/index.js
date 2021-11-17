e143c3daf85cc20f5a8c6604d87fb23bebdbefa7
JoshDaos <joshuadaos1998@gmail.com>
2021-11-16 19:05:04 -0800

    added get request

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const UserModel = require('./models/Users');
 
 mongoose.connect('mongodb+srv://user123:test123@mern-tut.lx6oz.mongodb.net/merntutorial?retryWrites=true&w=majority');
 
+app.get('/getUsers', (req, res) => {
+    UserModel.find({}, (err, result) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
 
+// app.post()
 
 
 app.listen(7001, () => {
