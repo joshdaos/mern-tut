2be51b9bc099347418b89384701601c3ef644ea2
JoshDaos <joshuadaos1998@gmail.com>
2021-11-16 22:34:20 -0800

    added createUser func in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,25 @@ import Axios from 'axios';
 
 function App() {
   const [listOfUsers, setListOfUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [username, setUsername] = useState("");
 
   useEffect(() => {
     Axios.get("http://localhost:7001/getUsers").then((response) => {
       setListOfUsers(response.data);
-    })
+    });
   }, []);
+
+  const createUser = () => {
+    Axios.post("http://localhost:7001/createUser", {
+      name, 
+      age, 
+      username,
+    }).then((response) => {
+      alert("User created");
+    });
+  };
 
   return (
     <div className="App">
@@ -24,6 +37,35 @@ function App() {
           );
         })}
       </div>
+
+      <div>
+        <input 
+        type="text" 
+        placeholder="Name" 
+        onChange={(event) => {
+          setName(event.target.value);
+        }} 
+        />
+
+        <input 
+        type="number" 
+        placeholder="Age"
+        onChange={(event) => {
+          setAge(event.target.value);
+        }} 
+        />
+
+        <input 
+        type="text" 
+        placeholder="Username"
+        onChange={(event) => {
+          setUsername(event.target.value);
+        }} 
+        />
+
+        <button onClick={createUser}> Create User </button>
+      </div>
+
     </div>
   );
 }
