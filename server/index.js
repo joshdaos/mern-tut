b6023b70f43d344ab6578df8322e65f1031f73d8
JoshDaos <joshuadaos1998@gmail.com>
2021-11-16 21:38:06 -0800

    added a post route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const app = express();
 const mongoose = require('mongoose');
 const UserModel = require('./models/Users');
 
+// parses the body
+app.use(express.json());
+
 mongoose.connect('mongodb+srv://user123:test123@mern-tut.lx6oz.mongodb.net/merntutorial?retryWrites=true&w=majority');
 
 app.get('/getUsers', (req, res) => {
@@ -15,7 +18,13 @@ app.get('/getUsers', (req, res) => {
     });
 });
 
-// app.post()
+app.post('/createUser', async (req, res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+
+    res.json(user);
+})
 
 
 app.listen(7001, () => {
