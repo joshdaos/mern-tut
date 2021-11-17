acb40d95f89562075b28de90b6c23bcf92570fb6
JoshDaos <joshuadaos1998@gmail.com>
2021-11-16 18:35:47 -0800

    created models

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://user123:test123@mern-tut.lx6oz.mongodb.net/merntutorial?retryWrites=true&w=majority');
 
+
+
+
 app.listen(7001, () => {
     console.log('Server is running..')
 });
